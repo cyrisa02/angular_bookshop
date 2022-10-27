7f1f5ed98b96ee37685ef031dce0786950d99a70
Cyril Gourdon <cyril.gourdon.02@gmail.com>
2022-10-27 15:40:17 +0200

    Add BooksModul

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,20 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BooksComponent } from './books/books.component';
-import { BookComponent } from './book/book.component';
+import { BooksModule } from './books/books.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BooksComponent,
-    BookComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, FormsModule
+    AppRoutingModule, FormsModule, BooksModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/books/book/book.component.ts
+++ src/app/books/book/book.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { Book } from '../types/Book';
+import { Book } from 'src/app/types/Book';
+
 
 @Component({
   selector: 'app-book',
