0b1db1115da4314c5d44f2ebdb2f8e0cf2bd3b9a
Cyril Gourdon <cyril.gourdon.02@gmail.com>
2022-10-28 11:57:24 +0200

    Check for authentification and logout

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginForm } from 'src/app/types/Auth';
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -11,31 +11,19 @@ export class LoginComponent implements OnInit {
   form: LoginForm = {
     email: '',
     password: '',
-    confirm_password: '',
+
   }
 
-  isLoading: boolean = false;
-
-
-
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
   submit() {
-    if (this.isLoading) return;
+    this.authService.login(this.form);
+  }
 
-    this.isLoading = true;
-    const auth = getAuth();
-    signInWithEmailAndPassword(auth, this.form.email, this.form.password)
-      .then((userCredential) => {
-        alert('Yeahhh login success');
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        alert('Credentials do not match our record.')
-      }).finally(() => (this.isLoading = false));
+  isLoading() {
+    return this.authService.isLoading;
   }
 }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { LoginForm } from 'src/app/types/Auth';
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { RegisterForm } from 'src/app/types/Auth';
+
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-register',
@@ -8,38 +9,21 @@ import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  form: LoginForm = {
+  form: RegisterForm = {
     email: '',
     password: '',
     confirm_password: '',
   }
 
-  passwordMatched: boolean = true;
-  isLoading: boolean = false;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
   submit() {
-    if (this.isLoading) return;
-
-    this.isLoading = true;
-
-    if (this.form.password != this.form.confirm_password) {
-      this.passwordMatched = false;
-      return;
-    }
-
-    const auth = getAuth();
-    createUserWithEmailAndPassword(auth, this.form.email, this.form.password)
-      .then((userCredential) => {
-
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ..
-      }).finally(() => (this.isLoading = false));
+    this.authService.register(this.form)
+  }
+  isLoading() {
+    return this.authService.isLoading;
   }
 }
